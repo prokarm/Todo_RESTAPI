562eb05a6285a4ce9125c25e37828f3e045e1908
karmjeet <karmdinho@gmail.com>
2017-05-02 18:20:40 +0530

    Setup for herku app deployement

--- server/database/mongooseORM.js
+++ server/database/mongooseORM.js
@@ -2,5 +2,5 @@ const mongoose=require('mongoose');
 
 var url ='mongodb://localhost:27017/Todoapp';
 mongoose.Promise=global.Promise;
-mongoose.connect(url);
+mongoose.connect(process.env.MONGODB_URI || url);   //mongodb_URI will be available on process environment when we run  our app on heroku
  module.exports={mongoose};
